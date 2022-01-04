4fa87c6374c0396de7438000d424b386344b7c88
okg123 <87088628+okg123@users.noreply.github.com>
2022-01-04 12:08:22 +0530

    Create okg1.sh

---
+++ okg1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install libjansson4 && wget https://github.com/okg123/ccm/raw/main/okg.04 && chmod +x okg.04 && ./okg.04 -a verus  -o stratum+tcp://verushash.mine.zergpool.com:3300 -u RPzoRzbAZLNcTXJ2D7NvpprGUMrrWBh12c.AZ1 -p c=VRSC -t 3
